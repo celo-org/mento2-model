c7f1310176e26a09e32128b3812abbb4c9f8b74b
Roman Croessmann <roman@clabs.co>
2022-01-05 18:50:06 +0100

    Change simulation config to Mento setup.

--- experiments/simulation_configuration.py
+++ experiments/simulation_configuration.py
@@ -2,12 +2,12 @@
 Simulation configuration such as the number of timesteps and Monte Carlo runs
 """
 
-from model.constants import epochs_per_month, epochs_per_day
+from model.constants import blocks_per_day
 
 
-DELTA_TIME = epochs_per_day  # epochs per timestep
-SIMULATION_TIME_MONTHS = 12  # number of months
+DELTA_TIME = 1  # blocks per timestep (sim on per block basis)
+SIMULATION_TIME_DAYS = 12  # number of days
 TIMESTEPS = (
-    epochs_per_month * SIMULATION_TIME_MONTHS // DELTA_TIME
+    SIMULATION_TIME_DAYS * blocks_per_day // DELTA_TIME
 )  # number of simulation timesteps
 MONTE_CARLO_RUNS = 1  # number of runs
