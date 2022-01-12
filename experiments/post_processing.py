cd24191a81a7d8979956c8311f3cfde980bcf827
Roman Croessmann <roman@clabs.co>
2022-01-12 19:11:59 +0100

    Add only grid parameters to results df.

--- experiments/post_processing.py
+++ experiments/post_processing.py
@@ -11,16 +11,19 @@ def assign_parameters(df: pd.DataFrame, parameters: Parameters, set_params=[]):
 
         for subset_index in df['subset'].unique():
             for (key, value) in parameter_sweep[subset_index].items():
+                print(key)
+                print(value)
                 df.loc[df.eval(f'subset == {subset_index}'), key] = value
-
     return df
 
 
 def post_process(df: pd.DataFrame, drop_timestep_zero=True, parameters=parameters):
-    # Assign parameters to DataFrame
-    assign_parameters(df, parameters, [
-        # Parameters to assign to DataFrame
-        'dt'
-    ])
+    # Show parameters that have taken more than one value in df
+    grid_keys = []
+    for key, value in parameters.items():
+        if len(value) > 1:
+            grid_keys.append(key)
+
+    assign_parameters(df, parameters, grid_keys)
 
     return df
