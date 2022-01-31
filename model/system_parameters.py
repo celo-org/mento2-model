da5d4fb74aebd2adc95e35db738dc62e5107c7e2
tobikuhlmann <tobias.kuhlmann@clabs.co>
2022-01-31 13:13:36 +0100

    update exchange spread parameter

--- model/system_parameters.py
+++ model/system_parameters.py
@@ -55,7 +55,7 @@ class Parameters:
     # Mento1.0-related parameters
     cusd_demand: List[float] = default([10000000])
     reserve_fraction: List[float] = default([0.01])
-    spread: List[float] = default([0.005])
+    spread: List[float] = default([0.0025])
     max_sell_amount: List[float] = default([0.10])
     bucket_update_frequency_seconds: List[int] = default([5 * 60])
 
