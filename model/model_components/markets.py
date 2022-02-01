686da09aff6f161e7dfca5bf3a6410f08ae04627
Nadiem Sissouno <nadiem.sissouno@celo.org>
2022-02-01 17:14:10 +0100

    WIP for discussion

--- model/model_components/markets.py
+++ model/model_components/markets.py
@@ -7,7 +7,6 @@ from pstats import Stats
 import random
 import numpy as np
 from stochastic import processes
-from statsmodels.distributions.empirical_distribution import ECDF
 import experiments.simulation_configuration as simulation_configuration
 from model.constants import blocktime_seconds
 from data import historical_values
@@ -25,76 +24,112 @@ class PriceImpact(Enum):
     CONSTANT_PRODUCT = 'constant_product'
 
 
+class ImpactDelay(Enum):
+    INSTANT = 'instant'
+
+
 class MarketPriceGenerator():
-    def __init__(self, model, drift, volatility, price_impact_model=PriceImpact.CONSTANT_PRODUCT, increments=[None]):
+    # Todo multi currency
+    # Todo in particular delay for Celo supply
+    # Todo typing
+    # Todo All seeds as params 
+    # Todo check thhat not 'entire' object bis saved in state history
+
+    def __init__(self, model, drift, volatility,
+                 price_impact_model=PriceImpact.CONSTANT_PRODUCT,
+                 historical_data=[None], increments=[None],
+                                  ):
         self.price_impact_model = price_impact_model
         self.model = model
         self.drift = drift
         self.volatility = volatility
-        self.increments = self.gbm_increments()
+        self.historical_data = historical_data
+        self.increments = increments
+        self.supply_changes = np.zeros(simulation_configuration.DELTA_TIME * simulation_configuration.TIMESTEPS)
+        # todo replace step by corresponding simulation counter 
         self.step = 0
 
     @classmethod
     def parse_from_parameters(cls, parameters):
         if parameters['model'][0] == MarketPriceModel.GBM:
-            return cls(parameters['model'][0],
-                       parameters['drift_market_price'][0],
-                       parameters['volatility_market_price'][0])
+            print()
+            market_price_generator = cls(parameters['model'][0],
+                                         parameters['drift_market_price'][0],
+                                         parameters['volatility_market_price'][0])
+            market_price_generator.generate_gbm_increments()
+            return market_price_generator
         elif parameters['model'][0] == MarketPriceModel.PRICE_IMPACT:
             return cls(parameters['model'][0],
                        None,
                        None)
+        elif parameters['model'][0] == MarketPriceModel.HIST_SIM:
+            market_price_generator = cls(None,
+                                         None,
+                                         None)
+            market_price_generator.load_historical_data(parameters['data_file'][0])
+            return market_price_generator
 
-    @classmethod
-    def load_historical_data(cls, file):
+    def load_historical_data(self, file):
+        """
+        Parser to read prices and turn them into log-returns"""
         return None
 
     def observe_market_states(self, state):
+        """
+        !!! probably not needed!!!
+        """
         return {'virtual_tank': state['virtual_tanks']['usd'],
                 'supply': state['floating_supply']['cusd'],
                 'market_price': state['market_price']['cusd_usd']}
 
     def market_price(self, state):
-        if self.model == MarketPriceModel.GBM:
-            self.step = + 1
-            return state['market_price']['cusd_usd'] * self.increments[self.step]
+        step = state['timestep']
+        if self.model == MarketPriceModel.GBM:       
+            return state['market_price']['cusd_usd'] * self.increments[step]
         elif self.model == MarketPriceModel.PRICE_IMPACT:
-            return self.valuate(state['supply'], state['virtual_tank'])
+            return self.valuate_price_impact(state['supply'], state['virtual_tank'])
         elif self.model == MarketPriceModel.HIST_SIM:
-            self.step = + 1
-            return state['market_price']['cusd_usd']*self.increments[self.step]
+            return state['market_price']['cusd_usd'] * self.increments[step]
 
     def price_impact_function(self, mode):
         return lambda asset_1, asset_2: asset_1 / asset_2
         # elif mode == PriceImpact.CONSTANT_FIAT
         #    return lambda
 
-    def valuate(self, supply, virtual_tank):
-        return self.price_impact_function(self.price_impact_model)(supply, virtual_tank)
+    def valuate_price_impact(self, supply, virtual_tank):
+        delayed_supply = self.impact_delay(supply)
+        return self.price_impact_function(self.price_impact_model)(delayed_supply, virtual_tank)
 
-    def gbm_increments(self, dt=simulation_configuration.DELTA_TIME,
-                       timesteps=simulation_configuration.TIMESTEPS):
+    def generate_gbm_increments(self, dt=simulation_configuration.DELTA_TIME,
+                                timesteps=simulation_configuration.TIMESTEPS):
+        # TODO remove hardcoded blocks per year use simulation_configuration
         blocks_per_year = 365 * 24 * 60 * 60 // blocktime_seconds
         timesteps_per_year = blocks_per_year // dt
         per_timestep_volatility = self.volatility / np.sqrt(timesteps_per_year)
+        print(self.drift)
         process = processes.continuous.GeometricBrownianMotion(
             drift=self.drift,
             volatility=per_timestep_volatility,
             t=1,
             rng=np.random.default_rng(1)
         )
+        self. increments = process.sample(timesteps * dt + 1)[1:]
 
-        increments = process.sample(timesteps * dt + 1)[1:]
-        return increments
     # Todo add delay that creates time lag between Mento trades and price impact
 
-    def impact_delay():
-        return None
+    def impact_delay(self,block_supply_change, impact_delay = ImpactDelay.INSTANT, dt=simulation_configuration.DELTA_TIME, timesteps=simulation_configuration.TIMESTEPS):
+        if impact_delay == ImpactDelay.INSTANT:
+            unit_array = np.zeros(timesteps * dt + 1)
+            delay_envelope = lambda x: x * unit_array
+            self.supply_changes += delay_envelope(block_supply_change)
 
     def historical_returns(self, sample_size):
+        """Creates a random sample from a set of historical log-returns
+        """
+    # TODO Consider different sampling options  
+        sample = random.choices(self.historical_data, sample_size)
 
-        random.choices(self.historical_data, sample_size)
-        return
+        self.increments = sample
 
     @property
     def cusd_usd_price(self):
--- model/parts/market_prices.py
+++ model/parts/market_prices.py
@@ -1,12 +1,35 @@
 """
-# Mento1 related policy and state update functions
+# Market Price related Update and Policy functions
 
-Calculation of changes in Mento buckets sizes, floating supply and reserve balances.
+
 """
+
+
+from sklearn.random_projection import SparseRandomProjection
+from model.types import VirtualTanks
 
 
 def p_market_price(params, substep, state_history, prev_state):
     market_price = {
         'cusd_usd':  prev_state['_market_price_generator'].market_price(prev_state)}
+    virtual_tanks = {'usd': prev_state['floating_supply']
+                     ['cusd'] * market_price['cusd_usd']}
     return {'market_price': market_price,
-            'virtual_tanks': {'usd': prev_state['virtual_tanks']['usd']}}
+            'virtual_tanks': virtual_tanks}
+
+def p_effective_supply(params, substep, state_history, prev_state):    
+    """
+    This function adds the delayed accumulated supply of the current step to the floating supply
+    """
+    temp = prev_state['floating_supply']['cusd'] - state_history[-1][-1]['floating_supply']['cusd']
+    print(temp)
+    # _market_price_generator'].valuate_price_impact()
+    return {'effective_supply':{'cusd': None}}
+
+def p_market_price_impact(params, substep, state_history, prev_state):
+    """
+    This function calculates the new market price based on the additional supply from p_supply_release
+    can be combined in one function
+    """
+    prev_state['_market_price_generator'].valuate(prev_state['floating_supply'], prev_state['virtual_tanks']['usd'])
+
