f72af470ae8a58802a9c81329c050dbece075e40
Tobias Kuhlmann <tobias.kuhlmann@clabs.co>
2022-02-08 20:22:12 +0100

    fix blocks_per_day (#11)

--- model/constants.py
+++ model/constants.py
@@ -5,6 +5,10 @@ Constants used in the model e.g. number of epochs in a year, Gwei in 1 CELO
 gwei = 1e9
 wei = 1e18
 blocktime_seconds = 5
-blocks_per_day = 24 * 60 // blocktime_seconds
+blocks_per_day = 24 * 60 * 60 // blocktime_seconds
 blocks_per_month = blocks_per_day * 30
 blocks_per_year = blocks_per_month * 12
+blocks_per_epoch = blocks_per_day
+
+celo_genesis_supply = 600000000
+celo_max_supply = 1000000000
