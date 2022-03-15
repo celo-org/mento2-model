28f312f9153a974e33b669a5f9dc447126b86b0d
sissnad <53600650+sissnad@users.noreply.github.com>
2022-03-15 10:28:30 +0100

    Read Historical Data Only Once

--- model/utils.py
+++ model/utils.py
@@ -5,15 +5,18 @@ Misc. utility and helper functions
 import copy
 from dataclasses import field
 from functools import partial
+import numpy as np
+
+import pandas as pd
 
 
 def _update_from_signal(
     state_variable,
     signal_key,
-    params,
-    substep,
-    state_history,
-    previous_state,
+    _params,
+    _substep,
+    _state_history,
+    _previous_state,
     policy_input,
 ):
     return state_variable, policy_input[signal_key]
@@ -45,8 +48,31 @@ def default(obj):
     return field(default_factory=lambda: copy.copy(obj))
 
 
-class data_feed():
+# pylint: disable=too-few-public-methods
+class DataFeed:
     """
     Performs data conversion for generators
     """
+
     # TODO Build data_feed
+    def __init__(self, data_folder, data=None, length=0):
+        self.data_folder = data_folder
+        self.data = data
+        self.length = length
+
+    def load_historical_data(self, file_name):
+        """
+        Parser to read prices and turn them into log-returns"""
+        # TODO parser
+
+        if file_name[-3:] == "csv":
+            historical_data = pd.read_csv(self.data_folder + file_name)
+        elif file_name[-3:] == "prq":
+            historical_data = pd.read_parquet(self.data_folder + file_name)
+        self.data = np.array(historical_data)
+        self.length = len(historical_data)
+
+# TODO Quick and dirt hack to load historical data only once
+DATA_FOLDER = '/Users/sissnad/celo/reviews/mento2-model/data/'
+data_feed = DataFeed(DATA_FOLDER)
+data_feed.load_historical_data('mock_logreturns.csv')
