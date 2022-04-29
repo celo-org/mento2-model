7bca9a5a5472fd256e4c7e4a3821722c918a7dcd
tobikuhlmann <tobias.kuhlmann@clabs.co>
2022-04-29 10:11:59 +0200

    example visualizations

--- experiments/notebooks/visualizations/__init__.py
+++ experiments/notebooks/visualizations/__init__.py
@@ -14,7 +14,38 @@ from experiments.notebooks.visualizations.plotly_theme import (
 pd.options.plotting.backend = "plotly"
 
 
-def plot_celo_price(df):
+def plot_celo_market_price(df):
 
-    fig = px.line(df, x='timestep', y='mento_rate', facet_col='subset')
+    fig = px.line(df, x='timestep', y='market_price_celo_usd', color='run', facet_col='subset',
+                  title='CELO price')
+    return fig
+
+def plot_celo_floating_supply(df):
+    """
+    Plot the celo floating supply for each subset (each parameter grid combination)
+    :param df: simulation output dataframe after postprocessing
+    :return: plotly express fig
+    """
+    fig = px.line(df, x='timestep', y='floating_supply_celo', color='run', facet_col='subset',
+                  title='Floating supply CELO')
+    return fig
+
+def plot_reserve_balance(df):
+    """
+    Plot the celo reserve balance for each subset (each parameter grid combination)
+    :param df: simulation output dataframe after postprocessing
+    :return: plotly express fig
+    """
+    fig = px.line(df, x='timestep', y='reserve_balance_celo', color='run', facet_col='subset',
+                  title='reserve balance CELO')
+    return fig
+
+def plot_mento_rate(df):
+    """
+    Plot the mento rate for each subset (each parameter grid combination)
+    :param df: simulation output dataframe after postprocessing
+    :return: plotly express fig
+    """
+    fig = px.line(df, x='timestep', y='mento_rate', color='run', facet_col='subset',
+                  title='Mento rate')
     return fig
