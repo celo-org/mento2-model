8cebd6a2fefd6dbb85dfe2c221c6b6a914a6cfe6
tobikuhlmann <tobias.kuhlmann@clabs.co>
2022-05-02 13:49:44 +0200

    delete unused cadLabs assets

--- experiments/notebooks/visualizations/__init__.py
+++ experiments/notebooks/visualizations/__init__.py
@@ -5,11 +5,6 @@ from datetime import datetime
 import pandas as pd
 import plotly.express as px
 
-from experiments.notebooks.visualizations.plotly_theme import (
-    cadlabs_colors,
-    cadlabs_colorway_sequence,
-)
-
 # Set plotly as the default plotting backend for pandas
 pd.options.plotting.backend = "plotly"
 
