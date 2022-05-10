bdddb1700e3f25ca2e1554bb4385c41545b9cb1d
bowd <bogdan@bowd.io>
2022-05-10 13:35:58 +0200

    Make variable naming more consisten

--- model/utils/generator.py
+++ model/utils/generator.py
@@ -3,7 +3,7 @@ The abstract Generator Class
 '''
 import logging
 from abc import ABC, abstractmethod
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, List
 
 def state_update_blocks(selector: str) -> Callable:
     """
@@ -12,7 +12,7 @@ def state_update_blocks(selector: str) -> Callable:
     """
     def decorator(method):
         method.__is_state_update_block_provider__ = True
-        method.__state_update_blocks_selector__ = selector
+        method.__state_update_block_provider_selector__ = selector
         return method
     return decorator
 
@@ -26,44 +26,44 @@ class Generator(ABC):
     for the type system.
     '''
 
-    __state_update_blocks__: Dict[str, Callable] = {}
-    __state_update_blocks_cached__ = False
+    __state_update_block_providers__: Dict[str, Callable] = {}
+    __state_update_block_providers_cached__ = False
 
     @classmethod
     @abstractmethod
     def from_parameters(cls, params) -> "Generator":
         pass
 
-    def state_update_blocks(self, selectors: Optional[str]):
+    def state_update_blocks(self, selectors: List[str]):
         """
         Either inject all state update blocks for a generator or
         matched based on the provided selectors
         """
-        if self.__state_update_blocks_cached__ is False:
-            self.__create_state_update_blocks_cache__()
+        if self.__state_update_block_providers_cached__ is False:
+            self.__cache_state_update_block_providers__()
 
-        state_update_blocks_to_inject = []
+        state_update_block_providers = []
         if not selectors:
-            state_update_blocks_to_inject = self.__state_update_blocks__.values()
+            state_update_block_providers = self.__state_update_block_providers__.values()
         else:
             for selector in selectors:
-                selector_blocks_builder = self.__state_update_blocks__.get(selector, None)
-                if selector_blocks_builder is None:
+                provider = self.__state_update_block_providers__.get(selector, None)
+                if provider is None:
                     logging.warning(
                         "No state_update_blocks for %s with selector:%s",
                         self.__class__.__name__,
                         selector
                     )
                 else:
-                    state_update_blocks_to_inject.append(selector_blocks_builder)
+                    state_update_block_providers.append(provider)
 
         return [
             state_update_block
-            for selector_blocks in map(lambda x: x(), state_update_blocks_to_inject)
+            for selector_blocks in [provider() for provider in state_update_block_providers]
             for state_update_block in selector_blocks
         ]
 
-    def __create_state_update_blocks_cache__(self):
+    def __cache_state_update_block_providers__(self):
         """
         On the first run this analyzes the methods in the object
         to create a cache of selector -> method for state_update_block providers.
@@ -73,5 +73,5 @@ class Generator(ABC):
             is_provider = callable(method) and \
                           getattr(method, '__is_state_update_block_provider__', False)
             if is_provider:
-                selector = method.__state_update_blocks_selector__
-                self.__state_update_blocks__[selector] = method
+                selector = method.__state_update_block_provider_selector__
+                self.__state_update_block_providers__[selector] = method
