8650cf06583fa376514f9859862c2ef7592db31a
Nadiem Sissouno <53600650+sissnad@users.noreply.github.com>
2022-06-13 12:59:44 +0200

    oracle and mento fix (#168)

--- model/types.py
+++ model/types.py
@@ -1,7 +1,7 @@
 """
 Various Python types used in the model
 """
-from typing import Any, NamedTuple, Set, TypedDict, Union
+from typing import Any, NamedTuple, TypedDict, Union
 from enum import Enum
 
 from model.entities.balance import Balance
@@ -32,9 +32,11 @@ Timestep = int
 Blocknumber = int
 Day = int
 
+
 class SerializableEnum(Enum):
     def __str__(self):
         return self.value
+
 
 class TraderType(Enum):
     """
@@ -44,6 +46,7 @@ class TraderType(Enum):
     RANDOM_TRADER = "RandomTrading"
     MAX_TRADER = "SellMax"
 
+
 class Stable(SerializableEnum):
     """
     Celo Stable assets
@@ -52,23 +55,28 @@ class Stable(SerializableEnum):
     CREAL = "creal"
     CEUR = "ceur"
 
+
 class CryptoAsset(SerializableEnum):
     CELO = "celo"
     ETH = "eth"
     BTC = "btc"
     DAI = "dai"
 
+
 class Fiat(SerializableEnum):
     USD = "usd"
     EUR = "eur"
     BRL = "brl"
+
 
 class MentoExchange(SerializableEnum):
     CUSD_CELO = "cusd_celo"
     CREAL_CELO = "creal_celo"
     CEUR_CELO = "ceur_celo"
 
+
 Currency = Union[Stable, Fiat, CryptoAsset]
+
 
 class Pair(NamedTuple):
     base: Currency
@@ -77,15 +85,18 @@ class Pair(NamedTuple):
     def __str__(self):
         return f"{self.base.value}_{self.quote.value}"
 
+
 class MentoBuckets(TypedDict):
     stable: float
     reserve_asset: float
+
 
 class TraderConfig(NamedTuple):
     trader_type: TraderType
     count: int
     balance: Balance
     exchange: MentoExchange
+
 
 class MentoExchangeConfig(NamedTuple):
     reserve_asset: CryptoAsset
@@ -96,11 +107,13 @@ class MentoExchangeConfig(NamedTuple):
     bucket_update_frequency_second: int
     max_sell_fraction_of_float: float
 
+
 class MarketPriceConfig(NamedTuple):
     pair: Pair
     process: Any
     param_1: float
     param_2: float
+
 
 class MarketPriceModel(Enum):
     QUANTLIB = "quantlib"
@@ -108,19 +121,24 @@ class MarketPriceModel(Enum):
     HIST_SIM = "hist_sim"
     SCENARIO = "scenario"
 
+
 class PriceImpact(Enum):
     ROOT_QUANTITY = "root_quantity"
     CUSTOM = "custom"
+
 
 class ImpactDelayType(Enum):
     INSTANT = "instant"
     NBLOCKS = "nblocks"
 
+
 class AggregationMethod(Enum):
     IDENTITY = 'indentity'
 
+
 class OracleType(Enum):
     SINGLE_SOURCE = 'single_source'
+
 
 class OracleConfig(NamedTuple):
     type: OracleType
@@ -129,7 +147,7 @@ class OracleConfig(NamedTuple):
     delay: int
     reporting_interval: int
     price_threshold: int
-    pairs: Set[Pair]
+
 
 class ImpactDelayConfig(NamedTuple):
     model: ImpactDelayType
