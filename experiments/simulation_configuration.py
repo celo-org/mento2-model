346992212d7e14aaabedb0da7fbfb92b36ef14d1
boqdan <304771+bowd@users.noreply.github.com>
2022-06-14 07:36:46 -0400

    Fix random number seeding (#175)
    * Fix random number seeding
    
    * Fix comments

--- experiments/simulation_configuration.py
+++ experiments/simulation_configuration.py
@@ -8,6 +8,6 @@ SIMULATION_TIME_DAYS = 1  # number of days
 # number of simulation_configuration timesteps
 TIMESTEPS = SIMULATION_TIME_DAYS * blocks_per_day // BLOCKS_PER_TIMESTEP
 TIMESTEPS_PER_YEAR = blocks_per_year // BLOCKS_PER_TIMESTEP
-MONTE_CARLO_RUNS = 1  # number of runs
+MONTE_CARLO_RUNS = 2  # number of runs
 DATA_SOURCE = 'historical'   # 'mock' or 'historical'
 TOTAL_BLOCKS = (BLOCKS_PER_TIMESTEP * TIMESTEPS) + 1
