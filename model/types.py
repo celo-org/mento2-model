d531c1f8fe5bbbe86264c6b94199524c32b12df2
Nadiem Sissouno <nadiem.sissouno@clabs.co>
2022-06-16 14:26:17 +0200

    exchange combinatorics

--- model/types.py
+++ model/types.py
@@ -66,17 +66,106 @@ class Pair(NamedTuple):
 
     def get_rate(self, prev_state) -> float:
         market_prices = prev_state['market_price']
+
         if self in market_prices:
             rate = market_prices.get(self)
+
+        elif isinstance(self.base, Fiat) and isinstance(self.quote, Fiat):
+            rate = self.fiat_to_fiat(prev_state)
+
+        elif isinstance(self.base, CryptoAsset) and isinstance(self.quote, CryptoAsset):
+
+            rate = self.crypto_to_crypto(prev_state)
+
+        elif isinstance(self.base, Stable) and isinstance(self.quote, Stable):
+
+            rate = self.stable_to_stable(prev_state)
+
+        elif isinstance(self.base, CryptoAsset) and isinstance(self.quote, Fiat):
+            rate = self.crypto_to_fiat(prev_state)
+
+        elif isinstance(self.base, CryptoAsset) and isinstance(self.quote, Stable):
+
+            rate = self.crypto_to_stable(prev_state)
+
+        elif isinstance(self.base, Stable) and isinstance(self.quote, Fiat):
+
+            rate = self.stable_to_fiat(prev_state)
+
         else:
-            pair_1 = [pair for pair in market_prices if pair.base == self.base]
-            assert len(pair_1) == 1, f'No or multiple pairs simulated for {self.base}'
-            pair_1 = pair_1[0]
-            pair_2 = Pair(CryptoAsset.CELO, pair_1.quote)
-            pair_3 = Pair(CryptoAsset.CELO, self.quote)
-            pair_1_2, rate_1_2 = pair_1.implicit_rate(
-                pair_2, prev_state, rate_only=False)
-            rate = pair_1_2.implicit_rate(pair_3, prev_state, self_rate=rate_1_2)
+            pair_reversed = Pair(self.quote, self.base)
+            rate = pair_reversed.get_rate(prev_state)**(-1)
+
+        return rate
+
+    def stable_to_stable(self, prev_state):
+        market_prices = prev_state['market_price']
+        pair_1 = [pair for pair in market_prices if pair.base == self.base]
+        assert len(pair_1) == 1, f'No or multiple pairs simulated for {self.base}'
+        pair_4 = [pair for pair in market_prices if pair.base == self.quote]
+        assert len(pair_4) == 1, f'No or multiple pairs simulated for {self.quote}'
+        pair_1 = pair_1[0]
+        pair_4 = pair_4[0]
+        pair_2 = Pair(CryptoAsset.CELO, pair_1.quote)
+        pair_3 = Pair(CryptoAsset.CELO, pair_4.quote)
+        pair_1_2, rate_1_2 = pair_1.implicit_rate(
+            pair_2, prev_state, rate_only=False)
+        pair_1_3, rate_1_3 = pair_1_2.implicit_rate(
+            pair_3, prev_state, self_rate=rate_1_2, rate_only=False)
+        rate = pair_1_3.implicit_rate(pair_4, prev_state, self_rate=rate_1_3)
+        return rate
+
+    def crypto_to_stable(self, prev_state):
+        market_prices = prev_state['market_price']
+        pair_2 = [pair for pair in market_prices if pair.base == self.quote]
+        assert len(pair_2) == 1, f'No or multiple pairs simulated for {self.base}'
+        pair_2 = pair_2[0]
+        if self.base is not CryptoAsset.CELO:
+            pair_1 = Pair(self.base, pair_2.quote)
+            rate_1 = pair_1.crypto_to_fiat(prev_state)
+
+            rate = pair_1.implicit_rate(pair_2, prev_state, self_rate=rate_1)
+        else:
+            pair_1 = Pair(
+                self.base, pair_2.quote)
+
+            pair_2 = Pair(self.quote, pair_1.quote)
+
+            rate = pair_1.implicit_rate(pair_2, prev_state)
+        return rate
+
+    def crypto_to_fiat(self, prev_state):
+        pair_1 = Pair(self.base, CryptoAsset.CELO)
+        rate_1 = pair_1.crypto_to_crypto(prev_state)
+        pair_2 = Pair(CryptoAsset.CELO, self.quote)
+
+        rate = pair_1.implicit_rate(
+            pair_2, prev_state, self_rate=rate_1)
+        return rate
+
+    def crypto_to_crypto(self, prev_state):
+        pair_1 = Pair(self.base, Fiat.USD)
+        pair_2 = Pair(self.quote, Fiat.USD)
+        rate = pair_1.implicit_rate(
+            pair_2, prev_state)
+        return rate
+
+    def fiat_to_fiat(self, prev_state):
+        pair_1 = Pair(CryptoAsset.CELO, self.base)
+        pair_2 = Pair(CryptoAsset.CELO, self.quote)
+        rate = pair_1.implicit_rate(
+            pair_2, prev_state)
+
+        return rate
+
+    def stable_to_fiat(self, prev_state):
+        market_prices = prev_state['market_price']
+        pair_1 = [pair for pair in market_prices if pair.base == self.base]
+        assert len(pair_1) == 1, f'No or multiple pairs simulated for {self.base}'
+        pair_1 = pair_1[0]
+        pair_2 = Pair(pair_1.quote, self.quote)
+        rate_2 = pair_2.fiat_to_fiat(prev_state)
+        rate = pair_1.implicit_rate(pair_2, prev_state,  other_rate=rate_2)
         return rate
 
     def implicit_rate(self, other: Pair, prev_state, self_rate=None, other_rate=None,
@@ -110,10 +199,6 @@ class Pair(NamedTuple):
         elif not rate_only:
             output = [Pair(base, quote), rate]
         return output
-
-    def __eq__(self, other):
-        equal_pairs = (self.base == other.base) and (self.quote == other.quote)
-        return equal_pairs
 
 
 class MentoBuckets(TypedDict):
